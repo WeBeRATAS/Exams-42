3086cbe543bd11458bca5754516462aa2d484f00
Rubén BM <150420895+WeBeRATAS@users.noreply.github.com>
2024-11-11 17:24:57 +0100

    Add files via upload

---
+++ rank02_exam/lvl4/ft_split/ft_split.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// contar palabras de string
+// reservar malloc con palabras
+// palabras delimitadas por spaces/tabs/\n at the end and start string
+// cadena = (char *)malloc(sizeof char *) * leng + 1 (reserva malloc)
+
+char    **ft_split(char *str)
+{
+
+	return (
+}
